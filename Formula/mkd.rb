11f9e320027a9261a12ab33054d2969e52f919b7
Gabriel Cunha <cunha.gabriel919@gmail.com>
2026-03-05 08:41:53 -0300

    Update mkd to v0.2.0

--- Formula/mkd.rb
+++ Formula/mkd.rb
@@ -1,8 +1,8 @@
 class Mkd < Formula
   desc "My awesome CLI tool"
   homepage "https://github.com/cunhazera/mkd"
-  url "https://github.com/cunhazera/mkd/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "21bb66e8d1a32319cd1d50ed3f7d116c2b64d7fe97fbbd3ca1661edc4a407371"
+  url "https://github.com/cunhazera/mkd/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "5bf8123d094799d21296c6fe1288739c34cc63f7e6d2308fbd44a816f7815a99"
   license "MIT"
 
   depends_on "go" => :build
